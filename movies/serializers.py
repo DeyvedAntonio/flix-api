047014bcbe03be9731053af1ccd8003d5d0f8d1f
Deyved Antonio <deyved.antonio@gmail.com>
2025-03-04 14:51:19 -0300

    feat: add average rating calculation to MovieSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,21 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
     

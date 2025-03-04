936317e133e156890db383712149237b122206be
Deyved Antonio <deyved.antonio@gmail.com>
2025-03-04 14:51:05 -0300

    fix: add missing comma in Actor URL patterns

--- actors/urls.py
+++ actors/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('actors/', views.ActorListCreateView.as_view(), name='actors-create-list')
-    path('actors/<int:pk>', views.ActorRetrieveUpdateDestroyView.as_view(), name='actors-retrive-update-destroy')
+    path('actors/', views.ActorListCreateView.as_view(), name='actors-create-list'),
+    path('actors/<int:pk>', views.ActorRetrieveUpdateDestroyView.as_view(), name='actors-retrive-update-destroy'),
 ]

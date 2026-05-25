8b8db2d2b088c7b37f029e9e332bcc681f9a7afd
Deyved Antonio <deyved.antonio@gmail.com>
2026-05-24 21:20:03 -0300

    feat: adicionando nova rota.

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from genres.views import genre_list_view
+from genres.views import genre_list_view, genre_detail_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('genres/', genre_list_view, name='genre-list'),
+    path('genres/', genre_list_view, name='genre-list'),  # type: ignore
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,38 @@
+import json
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from .models import Genre
 
 
+@csrf_exempt
 def genre_list_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [
+            {
+                'id': genre.id,  # type: ignore
+                'name': genre.name
+            } for genre in genres
+        ]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse(
+            {
+                'id': new_genre.id,  # type: ignore
+                'name': new_genre.name,
+            },
+            status=201,
+        )
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {
+        'id': genre.id,  # type: ignore
+        'name': genre.name,
+    }
     return JsonResponse(data)
